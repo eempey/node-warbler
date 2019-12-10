eb3a28efd64e031cf4bc3436b6436ed8f406ec2b
Erin Empey <erin.empey@sycle.net>
2019-12-09 17:54:00 -0800

    Logout, set auth token

--- warbler-client/src/containers/App.js
+++ warbler-client/src/containers/App.js
@@ -4,8 +4,20 @@ import { configureStore } from '../store';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './Navbar';
 import Main from './Main';
+import { setAuthorizationToken, setCurrentUser } from '../store/actions/auth';
+import jwtDecode from 'jwt-decode';
 
 const store = configureStore();
+
+if(localStorage.jwToken) {
+  setAuthorizationToken(localStorage.jwToken);
+  // prevent someone from manually tampering with the jwtToken in localstorage
+  try {
+    store.dispatch(setCurrentUser(jwtDecode(localStorage.jwToken)));
+  } catch(e) {
+    store.dispatch(setCurrentUser({}));
+  }
+}
 
 const App = () => (
   <Provider store={store}>
