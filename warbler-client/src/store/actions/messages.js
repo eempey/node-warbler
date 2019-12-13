84d41e2911d53c22b16bac6b137a322c412062ee
Erin Empey <erin.empey@sycle.net>
2019-12-12 19:03:41 -0800

    Add new message

--- warbler-client/src/store/actions/messages.js
+++ warbler-client/src/store/actions/messages.js
@@ -1,5 +1,5 @@
 import { apiCall } from '../../services/api';
-import { addError } from '.errors';
+import { addError } from './errors';
 import { LOAD_MESSAGES, REMOVE_MESSAGE } from '../actionTypes';
 
 export const loadMessages = messages => ({
@@ -9,9 +9,16 @@ export const loadMessages = messages => ({
 
 export const fetchMessages = () => {
     return dispatch => {
-        return apiCall('GET', '/api/messages')
-            .then(res => dispatch(loadMessages(res))
-            .catch(err => addError(err.message))
-            );
+        return apiCall('get', '/api/messages')
+            .then((res) => dispatch(loadMessages(res)))
+            .catch(err => addError(err.message));
     }
+}
+
+export const postNewMessage = text => (dispatch, getState) => {
+    let { currentUser } = getState();
+    const id = currentUser.user.id;
+    return apiCall("post", `/api/users/${id}/messages`, { text })
+        .then(response => {})
+        .catch(err => dispatch(addError(err.message)));
 }
